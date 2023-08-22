7d3a162567fb8cebf7b86e7dbe032f92cf1f9779
DAVID <nabijonovo23@gmail.com>
2023-08-22 17:04:13 +0300

    project updated

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -9,23 +9,50 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _controller = TextEditingController();
+  final FocusNode searchFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home Screen"),
-      ),
-      body: Container(
-        color: background,
-        child: Center(
-          child: Text(
-            "Home",
-            style: TextStyle(
-              color: Theme.of(context).primaryColor,
-              fontSize: 45,
-              fontWeight: FontWeight.w500,
-            ),
+        title: SearchBar(
+          constraints: const BoxConstraints(maxHeight: 56),
+          padding: const MaterialStatePropertyAll(EdgeInsets.only(left: 8)),
+          hintText: "Search",
+          focusNode: searchFocusNode,
+          controller: _controller,
+          leading: const Icon(Icons.search),
+          textStyle: MaterialStatePropertyAll(TextStyle(color: gray)),
+          onChanged: (value) {},
+          surfaceTintColor: const MaterialStatePropertyAll(Colors.white),
+          elevation: const MaterialStatePropertyAll(0.5),
+          shape: MaterialStatePropertyAll(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
           ),
+        ),
+        actions: [
+          Image.asset(
+            "assets/images/ic_menu.png",
+            width: 24,
+            height: 24,
+          ),
+          const SizedBox(width: 14)
+        ],
+      ),
+      body: GestureDetector(
+        onTap: () {
+          searchFocusNode.unfocus();
+        },
+        child: Container(
+          width: double.infinity,
+          color: background,
         ),
       ),
     );
