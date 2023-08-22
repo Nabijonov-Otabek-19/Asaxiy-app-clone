d83a396c027d4e56d88dc1d3c868e5465ca9d229
DAVID <nabijonovo23@gmail.com>
2023-08-22 12:44:03 +0300

    project updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'presentation/screens/screens.dart';
 import 'theme/colors.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
