318bd384ff93d34bf208fa67a16b1f3be6a86f0a
DAVID <nabijonovo23@gmail.com>
2023-08-25 18:43:31 +0300

    project updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:asaxiy_clone/main_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import 'presentation/screens/screens.dart';
 import 'theme/colors.dart';
